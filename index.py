ba28a33c035f9653b015a3a17ec3df878f01f609
Maner <842665591@qq.com>
2023-05-16 12:58:08 +0800

    auto push

--- index.py
+++ index.py
@@ -3,10 +3,12 @@ def job():
     path = os.getcwd()
     tranvese_folder(path)
 
-unlist= ['venv','.idea','.git','GitAutoPush.bat','index.py']
+unlist= ['venv','.idea','.git']
+unfile = ['index.py','GitAutoPush.bat']
 def tranvese_folder(path,d=None):
     for root,dirs,files in os.walk(path):
         dirs[:]=[d for d in dirs if d not in unlist]
+        files[:]=[file_name for file_name in files if file_name not in unfile]
         if d not in unlist:
             for file_name in files:
                 print(os.path.join(root,file_name))
