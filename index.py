4fb96e0591c1dbb9ce34d8ad4f01bb33795edc3b
Maner <842665591@qq.com>
2023-05-16 12:37:02 +0800

    auto push

--- index.py
+++ index.py
@@ -3,16 +3,13 @@ def job():
     path = os.getcwd()
     tranvese_folder(path)
 def tranvese_folder(path):
-    for file in os.listdir(path):
-        if 'venv' in file:
-            file.remove("venv")
-        abs_path = os.path.join(path,file)
-        if os.path.isdir(abs_path):
-            print("dir: ",abs_path)
-            tranvese_folder(abs_path)
-        else:
-            print("file: ",abs_path)
-
+    for root,dirs,files in os.walk(path):
+        if "venv" in dirs in files:
+            dirs.remove("venv")
+        if ".idea" in dirs in files:
+            dirs.remove(".idea")
+        for file_name in files:
+            print(os.path.join(root,file_name))
 schedule.every(0.1).minutes.do(job)
 while True:
     schedule.run_pending()
