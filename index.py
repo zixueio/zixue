091e82fdb6ab5812afe093ab2d038c0ecc13037e
Maner <842665591@qq.com>
2023-05-16 12:20:51 +0800

    auto push

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 import schedule,time,os
-def job():
-    breakpoint()
 
-def tranvese_folder(path):
+
+def job(path):
     for file in os.listdir(path):
         abs_path = os.path.join(path,file)
-        
+        if os.path.isdir(abs_path):
+            print("dir: ",abs_path)
+            job(abs_path)
+        else:
+            print("file: ",abs_path)
 
 schedule.every(0.1).minutes.do(job)
 while True:
