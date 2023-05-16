4ab5af872b476b5e0d4e16033a3900ec47ec430e
Maner <842665591@qq.com>
2023-05-16 12:47:59 +0800

    auto push

--- index.py
+++ index.py
@@ -2,10 +2,12 @@ import schedule,time,os
 def job():
     path = os.getcwd()
     tranvese_folder(path)
-def tranvese_folder(path):
+
+unlist= ['venv','.idea','.git','GitAutoPush.bat','index.py']
+def tranvese_folder(path,d=None):
     for root,dirs,files in os.walk(path):
-        dirs[:]=[d for d in dirs if d not in ['venv','.idea']]
-        if d not in ['venv','.idea']:
+        dirs[:]=[d for d in dirs if d not in unlist]
+        if d not in unlist:
             for file_name in files:
                 print(os.path.join(root,file_name))
 schedule.every(0.1).minutes.do(job)
