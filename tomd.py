33f9d3c364f7b4f0e6b40624c724534991d0145f
Maner <842665591@qq.com>
2023-05-25 17:50:11 +0800

    auto push

--- tomd.py
+++ tomd.py
@@ -2,25 +2,25 @@ from markdown import markdown
 from pygments import highlight
 from pygments. lexers import get_lexer_by_name
 from pygments. formatters import HtmlFormatter
-import re
 
-def convert_md_to_html (md_text):
-# Convert Markdown to HTML
-html = markdown (md_text)
+# Read text from Markdown file
+with open ('example.md', 'r') as f:
+    text = f.read ()
 
-# Find code blocks
-code_blocks = re. findall (r '<pre> <code class = "([^"] *) "> (. *?) </code> </pre> ', html, flags = re. DOTALL)
+# Use the Markdown library to convert text to HTML
+html = markdown (text)
 
-for lang, code in code_blocks:
-# Remove 'language-' prefix from language name
-lang = lang [9:]
-
-# Highlight code
-lexer = get_lexer_by_name (lang, stripall = True)
+# Use the Pygments library to obtain the highlighted lexer and formatter in Python syntax
+lexer = get_lexer_by_name ("python", stripall = True)
 formatter = HtmlFormatter ()
-code_highlighted = highlight (code, lexer, formatter)
 
-# Replace original code block with highlighted code
-html = html. replace ('<pre> <code class = "language-{}"> {} </code> </pre>'. format (lang, code), code_highlighted)
+# Find all the code blocks in HTML and highlight them using the Pygments Library.
+# Assume that the code block starts with 'Python and ends '''.
+code_blocks = re.findall(r'```python(.*?)```', html, re.DOTALL)
+for code in code_blocks:
+    highlighted_code = highlight (code, lexer, formatter)
+html = html. replace (f '``` python {code} ```', highlighted_code)
 
-return html
+# Write HTML to a new file
+with open('your_file.html ',' w ') as f:
+    f. write (html)
