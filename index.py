356cf2eea7dc98c8a03067bb6ff03ebad7ad5f68
Maner <842665591@qq.com>
2023-05-25 15:35:45 +0800

    auto push

--- index.py
+++ index.py
@@ -1,26 +1,35 @@
-import schedule,os
+import os
 
 unlist= ['venv','.idea','.git']
 unfile = ['index.py','GitAutoPush.bat']
-def job():
-    current_foler_path = os.getcwd()
-    index_file_path= os.path.join(current_foler_path,'index.html')
-    os.path.join(current_foler_path,'index.html')
-    with open('index.html','w', encoding='utf-8') as f:
-            f.write("<head><meta charset='UTF-8'></head>\n\n")
-    tranverse_folder(current_foler_path,index_file_path)
-    with open(index_file_path,'a') as f:
-        f.write("\n")
 
-def tranverse_folder(path,d=None,index_file='index.html'):
+def job():
+    current_folder_path = os.getcwd()
+    index_file_path= os.path.join(current_folder_path,'index.html')
+
+    with open(index_file_path,'w', encoding='utf-8') as f:
+        f.write("<!DOCTYPE html>\n")
+        f.write("<html>\n")
+        f.write("<head>\n")
+        f.write("<meta charset='UTF-8'>\n")
+        f.write("</head>\n")
+        f.write("<body>\n\n")
+
+    traverse_folder(current_folder_path, index_file_path)
+
+    with open(index_file_path, 'a', encoding='utf-8') as f:
+        f.write("</body>\n")
+        f.write("</html>\n")
+
+def traverse_folder(path, d=None, index_file='index.html'):
     for root,dirs,files in os.walk(path):
-        dirs[:]=[d for d in dirs if d not in unlist]
-        files[:]=[file_name for file_name in files if file_name not in unfile]
+        dirs[:] = [dir for dir in dirs if dir not in unlist]
+        files[:] = [file_name for file_name in files if file_name not in unfile]
         if d not in unlist:
             for file_name in files:
                 if file_name.endswith('.html'):
-                    relative_path=os.path.relpath(os.path.join(root,file_name),os.path.dirname(index_file))
-                    with open('index.html','a', encoding='utf-8') as f:
-                        f.write(f"<p><a href='{relative_path}'>{relative_path}</a></p>")
+                    relative_path = os.path.relpath(os.path.join(root, file_name), os.path.dirname(index_file))
+                    with open(index_file, 'a', encoding='utf-8') as f:
+                        f.write(f"<p><a href='{relative_path}'>{relative_path}</a></p>\n")
 
-schedule.run_pending()
+job()
